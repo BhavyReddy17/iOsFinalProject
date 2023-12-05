b561ad119de81218bcff60ce899598d4eccb4bd9
Bhavya Reddy <sankubhavya1@gmail.com>
2023-12-05 09:50:45 -0600

    Complete code

---
+++ Green Thumb/Green Thumb/Vcs/LoginSignup/LoginVC.swift
@@ -0,0 +1,43 @@
+
+import UIKit
+
+class LoginViewController: UIViewController {
+   
+   @IBOutlet weak var email: UITextField!
+   @IBOutlet weak var password: UITextField!
+   
+ 
+   @IBAction func onLogin(_ sender: Any) {
+       
+       if(email.text!.isEmpty) {
+           showAlert(message: "Please enter your email id.")
+           return
+       }
+       
+       if(self.password.text!.isEmpty) {
+           showAlert(message: "Please enter your password.")
+           return
+       }
+       
+     
+       DatabseHelper.shared.login(email: email.text!, password: self.password.text!)
+       
+   }
+   
+   
+   @IBAction func onLockButtonPressed(_ sender: UIButton) {
+       
+       self.password.isSecureTextEntry.toggle()
+      
+       
+       let buttonImageName = password.isSecureTextEntry ? "lock" : "lock.open"
+           if let buttonImage = UIImage(systemName: buttonImageName) {
+               sender.setImage(buttonImage, for: .normal)
+       }
+   }
+}
+
+
+
+
+
